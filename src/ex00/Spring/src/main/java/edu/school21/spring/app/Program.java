ca62aa4adf1efa86d0d66653444371cef601fbdd
Khashiev <nxashiev@gmail.com>
2024-10-02 22:54:14 +0300

    ex00

---
+++ src/ex00/Spring/src/main/java/edu/school21/spring/app/Program.java
@@ -0,0 +1,14 @@
+package edu.school21.spring.app;
+
+import edu.school21.spring.printer.Printer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Program {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        Printer printer = context.getBean("printerWithPrefixStandardUpper", Printer.class);
+
+        printer.print("Hello!");
+    }
+}
